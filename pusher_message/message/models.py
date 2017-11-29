adb6239d345af9ba4d0b4dd09e1680be0d879b5b
gmiller290488 <gmiller290488@gmail.com>
2017-11-29 09:59:46 +0000

    Pusher chat room base

---
+++ pusher_message/message/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.contrib.auth.models import User
+# Create your models here.
+class Conversation(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  message = models.CharField(blank=True, null=True, max_length=225)
+  status = models.CharField(blank=True, null=True, max_length=225)
+  created_at = models.DateTimeField(auto_now=True)
---
+++ pusher_message/message/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from pusher import Pusher
+from .models import *
+from django.http import JsonResponse, HttpResponse
+
+#instantiate pusher
+pusher = Pusher(app_id=u'437140', key=u'8d5530f7a775b908af25', secret=u'0a28298c21031bff305b', cluster=u'eu')
+# Create your views here.
+# add the login required decorator, so the method cannot be accessed without login
+
+@login_required(login_url='admin/login/')
+def chat(request):
+   return render(request, "chat.html");
+
+# use the csrf_exempt decorator to exempt this function from csrf checks
+@csrf_exempt
+def broadcast(request):
+# trigger the message, channel and event to pusher
+   pusher.trigger(u'a_channel', u'an_event', {u'name': request.user.username, u'message': request.POST['message']})
+   return HttpResponse("done");
+
